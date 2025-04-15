1332c029cea9c4b0b8c32c489d3b29d1be42ba4b
Viktor Klijn <viktor.klijn@student.hu.nl>
2025-04-15 16:45:43 +0200

    Removed boiler plate code. Added google font.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,12 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Stint_Ultra_Expanded } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const stintUltraExpanded = Stint_Ultra_Expanded({
+  variable: "--font-stint-ultra-expanded",
+  weight: ["400"],
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -20,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${stintUltraExpanded.variable} antialiased`}
       >
         {children}
       </body>
