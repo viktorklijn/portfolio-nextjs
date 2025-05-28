043de4ef6b5bc460b76bdc96913c9cadbf8b676a
Viktor Klijn <viktor.klijn@student.hu.nl>
2025-05-28 21:45:13 +0200

    Removed old styling and title for a clean start. Added simple navbar and started on hero section. Updated dependencies in package.json

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,27 +1,15 @@
-import TitleComponent from '@/ui/TitleComponent';
+import {RoundNav} from "@/ui/RoundNav";
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-10 p-20 h-full">
-      <TitleComponent />
-      <div className="grid grid-cols-5 auto-rows-fr auto-cols-fr gap-5 h-full">
-        <div className="rounded-xl bg-foreground drop-shadow-sm drop-shadow-shade-1 p-10 row-span-1 col-span-4">
-          <h2>Skills</h2>
-        </div>
-        <div className="rounded-xl bg-foreground drop-shadow-sm drop-shadow-shade-1 p-10 col-start-5 row-span-2">
-          <h2>Certificates</h2>
-        </div>
+    <>
+      <RoundNav />
+      <header className="h-5/6 bg-secondary-background">
 
-        <div className="rounded-xl bg-foreground drop-shadow-sm drop-shadow-shade-1 p-10 row-span-2">
-          <h2>Projects</h2>
-        </div>
-        <div className="rounded-xl bg-foreground drop-shadow-sm drop-shadow-shade-1 p-10 col-span-1 row-span-1 ">
-          <h2>Images</h2>
-        </div>
-        <div className="rounded-xl bg-foreground drop-shadow-sm drop-shadow-shade-1 p-10 row-span-1 col-span-2">
-          <h2>Work experience</h2>
-        </div>
-      </div>
-    </main>
+      </header>
+      <main className="flex flex-col h-full">
+
+      </main>
+    </>
   );
 }
