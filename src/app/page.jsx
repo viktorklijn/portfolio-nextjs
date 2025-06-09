c16611b7a8dfb9c23724fa397815030f3f58cea7
Viktor Klijn <viktor.klijn@student.hu.nl>
2025-06-09 11:02:33 +0200

    Refactor header layout in Home component to improve structure and styling

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,7 +4,10 @@ export default function Home() {
   return (
     <>
       <RoundNav />
-      <header className="h-5/6 bg-secondary-background">
+      <header className="h-5/6 flex flex-row justify-center">
+        <div className="w-full h-full bg-secondary-background rounded-b-3xl shadow-sm">
+          <h1>Hero</h1>
+        </div>
 
       </header>
       <main className="flex flex-col h-full">
