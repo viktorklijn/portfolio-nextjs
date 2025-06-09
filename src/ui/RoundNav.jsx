503b902f30308353bb393ee0adf7c6b802893632
Viktor Klijn <viktor.klijn@student.hu.nl>
2025-06-09 11:02:26 +0200

    Update RoundNav component to change Contact button styling for consistency

--- src/ui/RoundNav.jsx
+++ src/ui/RoundNav.jsx
@@ -15,7 +15,7 @@ export const RoundNav = () => {
                     </div>
                     <div className=" w-1/2 flex flex-row justify-end items-center">
                         <a href=""
-                           className="px-4 py-2 rounded-xl bg-accent-1 hover:bg-accent-2 text-text-secondary font-semibold">Contact
+                           className="px-4 py-2 rounded-xl bg-secondary-background hover:bg-accent-2 text-text-primary font-semibold">Contact
                             me</a>
                     </div>
                 </div>
